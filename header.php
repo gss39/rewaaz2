02d4a0806ce9490790eda1db24e0baa5432c7abe
gurpreet <gss331729@gmail.com>
2024-09-25 09:01:38 +0530

    version 0.2

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Anon - eCommerce Website</title>
+  <title>Rewaaz - Best Brands & Best Quality & Best Price</title>
 
 
   <!-- ------------------------------------------------------------------------------- -->
@@ -33,7 +33,7 @@
   <!--
     - favicon
   -->
-  <link rel="shortcut icon" href="./assets/images/logo/favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="./assets/images/logo/logo.ico" type="image/x-icon">
 
   <!--
     - custom css link
@@ -42,14 +42,15 @@
   <!-- <link rel="stylesheet" href="{{ url_for('static', filename='style.css')}}"> -->
 
 
+
   <!--
     - google font link
   -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
     rel="stylesheet">
-
+ -->
 
 </head>
 
